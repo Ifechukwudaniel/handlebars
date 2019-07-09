2277448a1c7a803509fec0704bd0ddf379040869
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-07-09 05:27:00 +0100

    added bcryptjs to hash password and eddited uesr model

--- routes/home/index.js
+++ routes/home/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Post = require("../../models/Post")
 const Category = require("../../models/Category")
 const User = require("../../models/User")
+const bcrypt = require("bcryptjs")
 
 
 router.all('/*', (req, res, next) => {
@@ -50,13 +51,64 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', (req, res) => {
-    const newUser = new User({
-        firstname:req.body.firstname,
-        lastname:req.body.lastname,
-        email:req.body.email,
-        password:req.body.password
-    })
-    res.send(newUser);
+    let errors= []
+   if(!req.body.firstname){
+       errors.push({"message": "please enter a firstname"})
+   }
+   if(!req.body.lastname){
+    errors.push({"message": "please enter a lastname"})
+    }
+    if(!req.body.email){
+        errors.push({"message": "please enter a email"})
+    }
+    if(!req.body.password){
+        errors.push({"message": "please enter a password"})
+    }
+    if(!req.body.passwordConfirm){
+        errors.push({"message": "please enter a password confirm "})
+    }
+    if (req.body.password != req.body.passwordConfirm) {
+        errors.push({"message": "password did not match"})
+    } 
+    if(errors.length > 0){
+        res.render("home/register", {
+            errors: errors,
+            firstname:req.body.firstname,
+            lastname:req.body.lastname,
+            email:req.body.email
+        });
+
+    }
+    else{
+        User.findOne({email:req.body.email})
+        .then(user=>{
+          
+            if(!user){
+                const newUser = new User({
+                    firstname:req.body.firstname,
+                    lastname:req.body.lastname,
+                    email:req.body.email,
+                    password:req.body.password
+                })
+                 bcrypt.genSalt(10,(err,salt)=>{
+                     bcrypt.hash(newUser.password ,salt,(err, hash)=>{
+                         newUser.password =hash
+                         newUser.save()
+                         .then(data=>{
+                               req.flash("success_message",`Dear ${data.firstname} you have being registered you can now login`)
+                              res.redirect('/login/');
+                         })
+                     })
+                 })
+            }
+            else{
+                req.flash("error_message", "This User exist please login")
+                 res.redirect('/login/');
+            }
+        })     
+
+    }
+
 });
 
 
